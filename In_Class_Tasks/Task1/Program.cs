90c74ec2f4970a6fa0c30e1c18e33279b1c22999
Thatcher <tmw00037@mix.wvu.edu>
2026-08-20 12:41:04 -0400

    added task1

---
+++ In_Class_Tasks/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Thatcher Wilson. Today is Thursday 8/20/2026");
+        }
+    }
+}
