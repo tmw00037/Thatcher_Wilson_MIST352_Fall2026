22843881837e34d9cab3e257ad7ed63288077e32
Thatcher <tmw00037@mix.wvu.edu>
2026-09-03 12:50:21 -0400

    submitting current task3

---
+++ In_Class_Tasks/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+/*
+     * Name: Thatcher Wilson
+     * Section: 001 at 11:30 AM
+     * Assesment: Task3
+     * Date: 09/03/2026
+     */
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strMsg = "WAKE UP, Neo | Location: Zion | Time: 07:45 PM | Code: #NEO-7 | Pill: Red";
+            string[] splittedMsg = strMsg.Split('|');
+            string strPart1 = splittedMsg[0];
+            string strPart2 = splittedMsg[1];
+            string strPart3 = splittedMsg[2];
+            string strPart4 = splittedMsg[3];
+            string strPart5 = splittedMsg[4];
+            Console.WriteLine($"Alert:{strPart1.ToUpper().Replace(", ",",")}");
+            Console.WriteLine($"Agent:{strPart1.Split(',')[1]}");
+
+        }
+   }
+
